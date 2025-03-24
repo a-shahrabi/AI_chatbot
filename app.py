c8801945de3688d33cff344a921a265146dc9bd4
a-shahrabi <ardavan.shahrabi@dal.ca>
2025-03-23 23:33:38 -0400

    changes

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-from langchain.chat_model import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 
 load_dotenv()
 
 st.set_page_config(
-    
+
 )
